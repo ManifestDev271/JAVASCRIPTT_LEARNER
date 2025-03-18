b314b1645d1ad29000740167e73695839ed2dc9b
manifestdev <mohantypratik257@gmail.com>
2025-03-18 15:29:40 +0530

    Second Commit

---
+++ Day_02.js
@@ -0,0 +1,49 @@
+let x = 10;
+
+x += 5;  // Same as x = x + 5;
+console.log(x); // 15
+
+x -= 3;  // Same as x = x - 3;
+console.log(x); // 12
+
+x *= 2;  // Same as x = x * 2;
+console.log(x); // 24
+
+x /= 4;  // Same as x = x / 4;
+console.log(x); // 6
+
+x %= 5;  // Same as x = x % 5;
+console.log(x); // 1
+
+x **= 3; // Same as x = x ** 3;
+console.log(x); // 1 (since 1^3 = 1)
+
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+console.log(myStr);
+// Outputs: I am a "double quoted" string inside "double quotes".
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>'; // Change this line
+
+console.log(myStr);
+
+// Outputs: <a href="http://www.example.com" target="_blank">Link</a>
+
+var myStr =`"FirstLine\n\t\\SecondLine\nThirdLine"; ` // Change this line
+
+
+// length of the string
+
+var firstNameLength = 0;
+ var firName = "Ada";
+  
+
+ firstLetterOfFirstName = firName[0];
+ console.log(firstLetterOfFirstName); // Outputs: A
+
+
+    firstNameLength = firName.length;
+
+console.log(firstNameLength); 
+
+
