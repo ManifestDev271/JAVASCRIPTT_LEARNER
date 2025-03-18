f801897e052f233fca1abcee57b2f14f72f4cb31
manifestdev <mohantypratik257@gmail.com>
2025-03-18 13:32:26 +0530

    first commit

---
+++ Day_01.js
@@ -0,0 +1,140 @@
+console.log('Hello World!');
+
+
+// in line 1, we are using the console.log() method to print the string 'Hello World!' to the console.
+
+// To run the code, we need to execute the following command in the terminal:
+
+// Data Types and variable declaration in JavaScript
+
+// undefined,null,boollean,String,Symbol,Number,Object
+// let, const, var
+
+// let and const are block scoped, var is function scoped
+
+// let is used to declare a variable that can be reassigned, while const is used to declare a variable that cannot be reassigned.
+
+
+let name = 'John';
+
+console.log(name); 
+
+name = 'Doe';
+console.log(name);
+
+
+let name1 = 'raj';
+console.log(name1);
+
+
+name1= 'kumar';
+console.log(name1);
+
+// in let we can reassign the value as we can see in the above example
+
+const p1 = 85;
+console.log(p1);
+// p1= 90;
+// console.log(p1);
+
+// in const we cannot reassign the value as we can see in the above example
+
+
+var name2 = 'raj';
+console.log(name2);
+
+name2 = 'dhana';
+console.log(name2);
+
+// in var we can reassign the value as we can see in the above example
+
+
+// declaration and initialization of variables
+
+var a;
+console.log(a);
+// declaration of variable a
+
+var b = 2;
+console.log(b);
+// both intialization and declaration in a single line
+
+a =10;
+console.log(a);
+// initialization of variable a after declaration 
+a=54.87;
+console.log(a); 
+ // reassigning the value of a
+
+
+ let name4 = "Pratik";
+ console.log(name4);  // Correct
+
+
+ 
+//  console.log(Name2);  // Error: Name is not defined
+
+ var a = 10;
+ var b= 20;
+ var c = a+b;
+console.log(c);
+
+// add a+b and store the result in c
+
+var a = 10;
+var b= 20;
+var c = a-b;
+console.log(c);
+
+// subtract a-b and store the result in c
+
+var a = 10;
+var b= 20;
+var c = a*b;
+console.log(c);
+// multiply a*b and store the result in c
+
+var a = 10;
+var b= 20;
+var c = a/b;
+console.log(c);
+
+// divide a/b and store the result in c
+
+var myvar = 87;
+myvar++;
+console.log(myvar);
+
+// increment the value of myvar by 1
+
+var myvar = 87;
+myvar--;
+console.log(myvar);
+
+// decrement the value of myvar by 1
+
+var myvar = 87;
+
+myvar += 10;
+console.log(myvar);
+// increment the value of myvar by 10
+
+var myvar = 87;
+myvar -= 10;
+console.log(myvar);
+// decrement the value of myvar by 10
+
+
+var remainder;
+remainder = 11 % 3;
+console.log(remainder);
+// find the remainder of 11 divided by 3 and store the result in remainder
+
+
+ 
+
+
+
+
+
+
